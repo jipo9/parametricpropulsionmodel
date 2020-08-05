19146db3c9302d1e2ab85da39080254b52b6128b
jipo9 <67448313+jipo9@users.noreply.github.com>
2020-08-05 08:55:09 -0700

    Changes

--- Book Code/OffDesignV4.m
+++ Book Code/OffDesignV4.m
@@ -1,22 +1,23 @@
-%% Attempt at Off-Design based on flowchart
+%% Off Design V4
 clc
 clear all
 
 %% On Design Analysis
 [stateR,componentR,designR,performanceR,inputsR] = ondesign();
 
-%% Initial Guesses
 
+%% Initial Guesses
 f = stateR{9,4};
-alphap = .5;
 alpha = designR{2,2};
+alpha = .53;
 beta = designR{3,2};
 ep1 = designR{4,2};
 ep2 = designR{5,2};
 alphap = alpha/((1-beta-ep1-ep2)*(1+f) + ep1 + ep2);
-M6 = .4;
+M6 = .3835;
 M8 = .5;
 mdot0 = stateR{2,5};
+mdot0 = 85.602;
 
 %% Independent Variables
 
@@ -31,8 +32,7 @@ invar{7,2} = 1;
 
 %% Constant Declarations
 beta = designR{3,2};
-pi_d = componentR{3,2};
-pi_d = pi_d(2);
+pi_dmax = .96;
 eta_f = componentR{4,5};
 eta_cL = componentR{5,5};
 eta_cH = componentR{6,5};
@@ -41,30 +41,29 @@ ep1 = designR{4,2};
 ep2 = designR{5,2};
 eta_tH = componentR{9,5};
 eta_tL = componentR{12,5};
-M4 = 1;
-M45 = 1;
 pi_Mmax = componentR{14,2};
 A6 = 1; %placeholder
 A16 = 1; %placeholder
 A6A = 1; %placeholder
-pi_ABdry = 1;
 pi_AB = .95;
 pi_n = componentR{16,2};
 A8dry = 1; %placeholder
+eta_mPL = 1; %not sure where to get these
+eta_mPH = 1; %""
 
 %constants from on-design
 eta_cL = .8674;
 eta_mL = .995;
 eta_f = .8674;
 pi_b = .95;
-Cp_c = .24;
-Cp_t = .295;
+Cp_c = 1004.83; 
+Cp_t = 1235.11;
 eta_AB = .99;
 eta_cH = .8751;
 eta_mH = .995;
 eta_b = .999;
 gamma_c = 1.4;
-Cp_AB = .295;
+Cp_AB = 1235.11;
 eta_tH = .8995;
 eta_PL = 1;
 pi_c = 20;
@@ -74,202 +73,269 @@ gamma_AB = 1.3;
 eta_tL = .9074;
 eta_PH = 1;
 PtoL = designR{6,2};
+PtoH = designR{7,2};
 
 
 
-state = stateR;
-component = componentR;
-design = designR;
 
-%% Start of Loop 1
+%% Off Design Start
 
-alphaperr = 1;
-%while alphaperr > .001
-    
-To4 = state{9,3};
-mdot4 = state{9,5};
-mdot45 = state{12,5};
-Po4 = state{9,2};
-Po45 = state{12,2};
-To5 = state{13,3};
+[state,component,design,inputs] = offdesign();
+
+alt = inputs{2,2};
+M0 = inputs{3,2};
+[T0, ~, P0, ~] = atmosisa(alt); %obtain standard atmospheric conditions
+state(2,3) = {T0};
+state{2,4} = 0;
+[state] = unFAIR3(state,2);
+V0 = M0*sqrt(state{2,7}*state{2,10}*T0);
+
+ho0 = state{2,8}+(V0^2/2);
+state{3,8} = ho0;
+state{3,4} = 0;
+[state] = unFAIR3(state,3);
+
+h0 = state{2,8};
+tau_r = ho0/h0;
+Pro0 = state{3,2};
+Pr0 = state{2,2};
+pi_r = Pro0/Pr0;
+
+if M0<1
+    pi_d = pi_dmax;
+elseif M0>1 && M0<5
+    pi_d = pi_dmax * (1-.075*((M0-1)^1.35));
+else
+    pi_d = pi_dmax * (800/(M0^4 + 935));
+end
+
+state{4,8} = state{3,8};
+state{4,2} = state{3,2};
+pi_ABdry = 1-(1-pi_AB)/2;
 
 
-pi_tH = component{9,2};
-pi_tL = component{12,2};
-tau_tH = component{9,4};
-tau_m1 = component{8,4};
-tau_m2 = component{11,4};
-tau_tL = component{12,4};
 
-beta = design{3,2};
-ep1 = design{4,2};
-ep2 = design{5,2};
-f = state{9,4};
-PtoL = design{6,2};
-mdot0 = state{2,5};
+%% Set Initial Values
 
-mdot4_mdotc = (1-beta-ep1-ep2)*(1+f);
-tau_lambda = state{9,8}/state{2,8};
-tau_r = state{3,3}/state{2,3};
-pi_r = (1+((gamma_c-1)/2)*(invar{2,2})^2)^(gamma_c/(gamma_c-1));
-tau_cL = component{5,4};
-tau_cH = component{6,4};
-eta_mPL = 1;
-eta_mPH = 1;
-h0 = 1;
-tau_fR = componentR{4,4};
-tau_cLR = componentR{5,4};
+pi_tH = componentR{9,2};
+pi_tL = componentR{12,2};
+tau_tH = componentR{9,4};
+tau_tL = componentR{12,4};
+pi_f = componentR{4,2};
+pi_cL = componentR{5,2};
+pi_cH = componentR{6,2};
+tau_f = componentR{4,4};
+tau_cL = componentR{5,4};
+tau_cH = componentR{6,4};
+tau_m1 = componentR{8,4};
+tau_m2 = componentR{11,4};
+f = stateR{9,4};
+M4 = 1;
+M45 = 1;
+M6A = .42; %M6A = M6AR, need to get reference machs
+M8 = .9;  %M8 = M8R
 
-tau_f = 1+(((1-tau_tL)*eta_mL*((mdot4_mdotc*(tau_lambda*tau_tH/tau_r))+((ep1*tau_tH+ep2)*tau_cL*tau_cH)) - (((1+alpha)*PtoL)/(tau_r*eta_mPL*mdot0*h0))) / ((tau_cLR-1)/(tau_fR-1)+alpha));
+state{9,4} = f;
+[state] = unFAIR3(state,9);
+ho4 = state{9,8};
+ho45 = ho4*tau_m1*tau_tH*tau_m2;
+state{12,8} = ho45;
+f45 = f*(1-beta-ep1-ep2)/(1-beta);
+state{12,4} = f45;
+[state] = unFAIR3(state,12);
+ho5 = ho45*tau_tL;
+state{13,8} = ho5;
+state{13,4} = f45;
+[state] = unFAIR3(state,13);
 
-tau_cL = 1+(tau_f-1)*((tau_cLR-1)/(tau_fR-1));
+tau_mR = componentR{14,4};
+ho6A = ho5*tau_mR;
+f6A = f45*((1-beta)/(1+alpha-beta));
+state{15,4} = f6A;
+state{15,8} = ho6A;
+[state] = unFAIR3(state,15);
 
-ho13i = state{4,8}*(1+eta_f*(tau_f-1));
-state(5,8) = {ho13i};
-state(5,2) = {[]}; state(5,3) = {[]};
-[state] = unFAIR3(state,5);
-Pro13i = state{5,2};
-Pro2 = state{4,2};
-state(5,:) = stateR(5,:);
-pi_f = Pro13i/Pro2;
-
-ho25i = state{4,8}*(1+eta_cL*(tau_cL-1));
-state(6,8) = {ho25i};
-state(6,2) = {[]}; state(6,3) = {[]};
-[state] = unFAIR3(state,6);
-Pro25i = state{6,2};
-Pro2 = state{4,2};
-state(6,:) = stateR(6,:);
-pi_cL = Pro25i/Pro2;
-
-tau_cH = (1+((1-tau_tH)*eta_mH*((1-beta-ep1-ep2)*(1+f)*(tau_lambda/(tau_r*tau_cL)))) - (((1+alpha)/(tau_r*tau_cL*eta_mPH))*(PtoL/(mdot0*h0)))) / (1-ep1*(1-tau_tH)*eta_mH);
-
-ho3i = state{6,8}*(1+eta_cH*(tau_cH-1));
-state(7,8) = {ho3i};
-state(7,2) = {[]}; state(7,3) = {[]};
+%1
+ho3 = ho0*tau_cL*tau_cH;
+state{7,8} = ho3;
+state{7,4} = 0;
 [state] = unFAIR3(state,7);
-Pro3i = state{7,2};
+
+alphap = alpha/((1+f)*(1-beta-ep1-ep2)+ep1+ep2);
+
+state{9,2} = [];
+state{9,4} = f;
+state{9,8} = [];
+[state] = unFAIR3(state,9);
+f45 = f*(1-beta-ep1-ep2)/(1-beta);
+state{12,4} = f45;
+
+% Insert TURB AND TURBC FXs
+
+tau_cLR = componentR{5,2};
+tau_fR = componentR{4,4};
+
+
+tau_lambda = ho4/h0;
+tau_f = 1 ...
+    + ( (1-tau_tL)*eta_mL* (((1-beta-ep1-ep2)*(1+f)*tau_lambda*tau_tH/tau_r  +  (ep1*tau_tH + ep2)*tau_cL*tau_cH))   -   (1+alpha)*PtoL/(tau_r*eta_mPL*mdot0*h0)) ...
+    / ((tau_cLR - 1)/(tau_fR - 1) + alpha) 
+tau_cL = 1 + (tau_f -1)*(tau_cLR - 1)/(tau_fR - 1)
+tau_cH = 1 ...
+    + (1-tau_tH)*eta_mH* (((1-beta-ep1-ep2)*(1+f)*tau_lambda/(tau_r*tau_cL) + ep1*tau_cH))...
+    - (1+alpha)*PtoH/(tau_r*tau_cL*eta_mPH*mdot0*h0)
+
+ho2 = ho0;
+state{4,8} = state{3,8};
+Pro2 = Pro0;
+state{4,2} = state{3,2};
+ho13 = ho2*tau_f;
+state{5,8} = ho13;
+ho25 = ho2*tau_cL;
+state{6,8} = ho25;
+ho3 = ho25*tau_cH;
+state{7,8} = ho3;
+
+state{5,4} = 0;
+state{6,4} = 0;
+state{7,4} = 0;
+
+[state] = unFAIR3(state,5);
+[state] = unFAIR3(state,6);
+state{7,2} = [];
+state{7,3} = [];
+[state] = unFAIR3(state,7);
+
+
+statei = {'Station','Relative Pressure', ' Temperature (K)', 'Fuel to air ratio','Mass Flow (kg/s)','Cp (J/kg-K)', 'Gamma', 'Enthalpy (J/kg)', 'Entropy (J/kg-K)','Gas Constant (m^2/s^2*K)','Relative Density(kg/m^3)','Relative Volume(s*m^3??)'};
+statei(2:22,1) = {'0';'o0';'o2';'o13';'o2.5';'o3';'o3.1';'o4';'o4.1';'o4.4';'o4.5';'o5';'o6';'o6A';'o7';'o9';'9';'beta';'eptot';'ep1';'ep2'};
+ho13i = ho2*(1+eta_f*(tau_f-1));
+ho25i = ho2*(1+eta_cL*(tau_cL-1));
+ho3i = ho25*(1+eta_cH*(tau_cH-1));
+
+statei{5,8} = ho13i;
+statei{6,8} = ho25i;
+statei{7,8} = ho3i;
+statei{5,4} = 0;
+statei{6,4} = 0;
+statei{7,4} = 0;
+
+[statei] = unFAIR3(statei,5);
+[statei] = unFAIR3(statei,6);
+[statei] = unFAIR3(statei,7);
+
+Pro13i = statei{5,2};
+Pro25i = statei{6,2};
+Pro3i = statei{7,2};
+Pro2 = state{4,2};
 Pro25 = state{6,2};
-state(7,:) = stateR(7,:);
+
+pi_f = Pro13i/Pro2;
+pi_cL = Pro25i/Pro2;
 pi_cH = Pro3i/Pro25;
+pi_c = pi_cL*pi_cH;
+tau_c = tau_cL*tau_cH;
 
-%%
-
-% Po16_P16 = (pi_f/(pi_cL*pi_cH*pi_b*pi_tH*pi_tL))*(1+((gamma_t-1)/2)*M6^2)^(gamma_t/(gamma_t-1));
-% 
-% [M16] = Kutta_mach(gamma_c,M6,gamma_t,Po16_P16);
-% 
-% A16_A6 = .2715; %given by joey
-% [MFP16] = MFP2(M16,state{5,7},state{5,10});
-% [MFP6] = MFP2(M6,state{14,7},state{14,10});
-% To6 = state{14,3};
-% To16 = state{5,3};
-% alphapnew = (pi_f/(pi_cL*pi_cH*pi_b*pi_tH*pi_tL)) * (A16_A6) * (MFP16/MFP6) * sqrt(To6/To16);
-% alphaperr = norm((alphapnew-alphap)/alphap);
-% alpha = alphap*((1-beta-ep1-ep2)*(1+f)+ep1+ep2);
-% alphap = alphapnew;
-% end
-% 
-% 
-% M6err = 1;
-% while M6err > .0005
-%    
-% tau_M = (1+alphap*((tau_r*tau_f)/(tau_lambda*tau_m1*tau_tH*tau_m2*tau_tL)))/(1+(alphap*Cp_c/Cp_t));
-% 
-% R6 = state{14,10};
-% R16 = state{5,10};
-% Cp6A = state{15,6};
-% 
-% R6A = (R6+(alphap*R16))/(1+alphap);
-% gamma6A = Cp6A/(Cp6A-R6A);
-% gamma16 = state{5,7};
-% gamma6 = state{14,7};
-% 
-% phi1 = ((M6^2)*(1+(.5*(gamma6-1)*M6^2))) / (1+(gamma6*M6^2))^2;
-% phi2 = ((M16^2)*(1+(.5*(gamma16-1)*M16^2))) / (1+(gamma16*M16^2))^2;
-% 
-% 
-% phi = ((1+alphap)/(1/sqrt(phi1) + alphap*(sqrt(gamma6*R16/gamma16*R6)*sqrt((To16/To6)/phi2))))^2 * (gamma6*R6A*tau_M/(gamma6A*R6));
-% 
-% M6A = sqrt((2*phi)/((1-2*gamma6A)+sqrt(1-2*(gamma6A+1)*phi))); %doesn't work
-% 
-% M6A = .4188; % actual value
-% 
-% To6A = state{15,3};
-% f6 = state{14,4};
-% f6A = state{15,4};
-% A6_A6A = .729; %assumed value
-% pi_Mi = real((1+alphap) * sqrt(To6A/To6) * (A6_A6A)  * (MFP2(M6,To6,f6)/MFP2(M6A,To6A,f6A)));
-% pi_M = pi_Mmax*pi_Mi;
-% 
-% 
-% Po9_P0dry = pi_r*pi_d*pi_cL*pi_cH*pi_b*pi_tH*pi_tL*pi_M*pi_ABdry*pi_n;
-% 
-% M9 = sqrt((((Po9_P0dry)^(gamma6A-1/gamma6A))-1)*(2/(gamma6A-1)));
-% 
-% if M9>=1
-%     M8 = 1;
-% else 
-%     M8 = M9;
-% end
-% 
-% A8dry_A6 = .3;
-% 
-% MFP8 = MFP2(M8,state{16,7},state{16,10});
-% MFP6 = pi_M*pi_ABdry*(A8dry_A6)*(MFP8/(1+alphap))*sqrt(To6/To6A);
-% 
-% fun = @(M6) M6*sqrt(gamma6/R6)*((1+((gamma6-1)/2)*M6^2)^((gamma6-1)/(2-2*gamma6))) - MFP6;
-% M6new = fzero(fun,M16); 
-% M6err = norm((M6new-M6)/M6);
-% if M6>M6new
-%     M6 = M6-.0001;
-% else
-%     M6 = M6+.002;
-% end
-% 
-% end
-% 
-% 
-% mdot0err = 1;
-% while mdot0err > .0005
-%     
-% A4 = 1;
-% P0 = state{23,1};
-% MFP4 = MFP2(M4,state{9,7},state{9,10});
-% mdot0new = ((1+alpha)*P0*pi_r*pi_d*pi_cL*pi_cH*pi_b*A4*MFP4 / ((1-beta-ep1-ep2)*(1+f)*sqrt(To4)));
-% mdot0err = norm((mdot0new-mdot0)/mdot0);
-% mdot0 = mdot0new;
-%  
-%    
-% end
-% 
-% P0_P9 = invar{7,2};
-% Po9_P9 = P0_P9*pi_r*pi_d*pi_cL*pi_cH*pi_b*pi_tH*pi_tL*pi_M*pi_AB*pi_n;
-% 
-% M9 = sqrt(((Po9_P9^((gamma_AB-1)/gamma_AB))-1)*(2/(gamma_AB-1)));
+%2
+ftemp = f;
 
 
-%% Other Functions
 
-function [M1] = Kutta_mach(gamma1,M2,gamma2,pi2_1)
-%calculates the mach if kutta condition is satisfied between the two states
-P_Pt2 = pressure(M2,gamma2);
-P_Pt1 = P_Pt2*pi2_1;
-M1 = sqrt((2/(gamma1 - 1)) * (P_Pt1 ^ ((gamma1 - 1)/gamma1)  - 1));
-end
-function [P_Pt] = pressure(M,gamma)
-P_Pt = (1 - (gamma - 1)/2*M^2)^(-gamma/(gamma-1));
-end
-function [T_Tt] = temperature(M,gamma)
-T_Tt = (1 - (gamma - 1)/2*M^2)^-1;
-end
-function [MFP] = MFP2(M, gamma, R)
-    [P_Pt] = pressure(M,gamma);
-    [T_Tt] = temperature(M,gamma);
-    MFP = M*sqrt(gamma/R)/sqrt(T_Tt)*P_Pt;
+
+
+%% Functions
+function [state,component,design,inputs] = offdesign()
+state = {'Station','Relative Pressure', ' Temperature (K)', 'Fuel to air ratio','Mass Flow (kg/s)','Cp (J/kg-K)', 'Gamma', 'Enthalpy (J/kg)', 'Entropy (J/kg-K)','Gas Constant (m^2/s^2*K)','Relative Density(kg/m^3)','Relative Volume(s*m^3??)'};
+state(2:22,1) = {'0';'o0';'o2';'o13';'o2.5';'o3';'o3.1';'o4';'o4.1';'o4.4';'o4.5';'o5';'o6';'o6A';'o7';'o9';'9';'beta';'eptot';'ep1';'ep2'};
+component = {'Component','Pressure Ratio','Polytropic Efficieny','Enthalpy Ratio', 'Mechanical/Combustion Efficiency'};
+component(2:17,1) = {'Ram Recovery';'Inlet(Ideal,Actual)';'Fan';'LP Compressor';'HP Compressor';'Main Burner';'Coolant Mixer 1';'HP Turbine';'HP Takeoff';'Coolant Mixer 2';'LP Turbine';'LP Takeoff';'Mixer';'Afterburner';'Nozzle';'Overall'};
+design = {'Parameter','Value'};
+design(2:8,1) = {'alpha';'beta';'epsilon1';'epsilon2';'PtoL';'PtoH';'h_PR'};
+inputs = {'Parameter','Value'};
+inputs(2:8,1) = {'Altitude (m)','Mach Number','F/mdot','Mass Flow Rate (kg/s)','SFC (kg/s/N)','Max Burner Temp (K)','Po9/P9'};
+
+% gamma_c = 1.4;
+% gamma_t = 1.3;
+% gamma_AB = 1.3;
+% cp_c = .24* 4186.8; %J/kg K
+% cp_t = .295* 4186.8; %J/kg K
+% cp_AB = .295* 4186.8; %J/kg K
+
+alt = 36000/3.281; %altitude [m from feet]
+M0 = 1.451; %freestream mach number
+F_mdot = 62.493*9.806655; %thrust/mdot [N/kg/s from lbf/(lbm/s)]
+mdot = 200*0.45359237; %freestream mass flow rate [kg/s from lbm/s]
+S = 1.0862*((.453592/3600)/4.44822); %specific fuel consuption[kg/s/N from lbm/(lbf/s)]
+To4 = 3200*.555556; %max burner temperature [R to K]
+Po9_P9 = 11.327; %stagnation to static pressure ratio at the nozzle (note: in future analysis calculated by total pressure ratio)
+
+alpha = .4; %bypass ratio (-1?)
+beta = .01; %bleed ratio
+PtoH = 300.61*10^3; %power takeoff high spool [W]
+PtoL = 0.000000001; %power takeoff low spool [W]
+h_PR = 18400*2326; %fuel heating value for a (CH2)n propellant [J/kg]
+
+pi_dmax = .96; %diffuser pressure ratio
+pif = 3.9; %fan pressure ratio
+ef = .89; %fan polytropic efficiency
+picL = 3.9; %low pressure compressor pressure ratio
+ecL = .89; %low pressure polytropic efficiency
+    picH = 5.1282; %high pressure compressor pressure ratio
+ecH = .9; %high pressure polytropic efficiency
+eta_b = .999; %burner efficiency
+pi_b = .95; % burner pressure ratio
+etH = .89; %high pressure turbine polytropic efficiency
+etL = .9; %low pressure turbine polytropic efficiency
+etamH = .995; %high pressure shaft mechanical efficiency
+etamPH = 1; %high pressure shaft power takeoff mechancal efficiency
+etamL = .995; %low pressure shaft mechanical efficiency
+etamPL = 1; %low pressure shaft power takeoff mechancal efficiency
+pi_M_max = .97; %mixer pressure ratio
+pin = .97; %nozzle pressure ratio
+
+
+% %Control limits
+% pi_c = 20;
+
+
+% Store all values
+design(2,2) = {alpha}; %store values in design
+design(3,2) = {beta};
+design(7,2) = {PtoH};
+design(6,2) = {PtoL};
+design(8,2) = {h_PR};
+state(9,3) = {To4};
+component(3,2) = {pi_dmax}; %store values in component
+component(4,2:3) = {pif,ef};
+component(5,2:3) = {picL,ecL};
+component(6,2:3) = {picH,ecH};
+component(7,2) = {pi_b};
+component(7,5) = {eta_b};
+component(9,5) = {etamH};
+component(10,5) = {etamPH};
+component(9,3) = {etH};
+component(12,5) = {etamL};
+component(13,5) = {etamPL};
+component(12,3) = {etL};
+component(14,2) = {pi_M_max};
+component(16,2) = {pin};
+component(2:3,3) = {1};
+component(7:8,3) = {1};
+component(11,3) = {1};
+component(14:16,3) = {1};
+inputs(2,2) = {alt};
+inputs(3,2) = {M0};
+inputs(4,2) = {F_mdot};
+inputs(5,2) = {mdot};
+inputs(6,2) = {S};
+inputs(7,2) = {To4};
+inputs(8,2) = {Po9_P9};
+
+%[state,design] = derived_parameters_parametric(state,inputs,design,component);
+
+
 end
 
-%% Parametric Functions
 function [stateR,componentR,designR,performanceR,inputsR] = ondesign()
 stateR = {'Station','Relative Pressure', ' Temperature (K)', 'Fuel to air ratio','Mass Flow (kg/s)','Cp (J/kg-K)', 'Gamma', 'Enthalpy (J/kg)', 'Entropy (J/kg-K)','Gas Constant (m^2/s^2*K)','Relative Density(kg/m^3)','Relative Volume(s*m^3??)'};
 stateR(2:22,1) = {'0';'o0';'o2';'o13';'o2.5';'o3';'o3.1';'o4';'o4.1';'o4.4';'o4.5';'o5';'o6';'o6A';'o7';'o9';'9';'beta';'eptot';'ep1';'ep2'};
@@ -287,8 +353,8 @@ inputsR(2:8,1) = {'Altitude (m)','Mach Number','F/mdot','Mass Flow Rate (kg/s)',
 % cp_t = .295* 4186.8; %J/kg K
 % cp_AB = .295* 4186.8; %J/kg K
 
-alt = 40000/3.281; %altitude [m from feet]
-M0 = 1.8; %freestream mach number
+alt = 36000/3.281; %altitude [m from feet]
+M0 = 1.451; %freestream mach number
 F_mdot = 62.493*9.806655; %thrust/mdot [N/kg/s from lbf/(lbm/s)]
 mdot = 200*0.45359237; %freestream mass flow rate [kg/s from lbm/s]
 S = 1.0862*((.453592/3600)/4.44822); %specific fuel consuption[kg/s/N from lbm/(lbf/s)]
@@ -887,4 +953,6 @@ eta_o = eta_TH*eta_P;
 performance(1,:) = {'Thrust','Specific Fuel Consumption','Propulsive Efficiency','Thermal Efficiency','Overall Efficiency','Exhaust Mach'};
 performance(2,:) = {F_mdot,S,eta_TH,eta_P,eta_o,M9};
 end
+
+
 
